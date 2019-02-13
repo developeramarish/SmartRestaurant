34f73431bb2b49b242260006c0637d286b410aa3
Görkem Öztürk <gorkemozturk@hotmail.com>
2019-02-13 15:49:17 +0300

    Initial models has been implemented, and client layer created.

---
+++ SmartRestaurant.Service/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.Service.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string OrderName { get; set; }
+
+        public string Total { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ SmartRestaurant.Service/Models/OrderProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.Service.Models
+{
+    public class OrderProduct
+    {
+        public int ID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
